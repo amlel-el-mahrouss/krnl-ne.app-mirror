251e0e9927a75f6193d9780d8a11aacb43111739
Amlal El Mahrouss <amlal@nekernel.org>
2026-02-21 21:26:11 +0100

    chore: libASN: start to implement the AudioSoundNamespace library.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

---
+++ src/libASN/src/VirtualMixer.cpp
@@ -0,0 +1,5 @@
+// Copyright 2026, Amlal El Mahrouss (amlal@nekernel.org)
+// Licensed under the Apache License, Version 2.0 (see LICENSE file)
+// Official repository: https://github.com/nekernel-org/nekernel
+
+#include <AsnKit/VirtualMixer.h>

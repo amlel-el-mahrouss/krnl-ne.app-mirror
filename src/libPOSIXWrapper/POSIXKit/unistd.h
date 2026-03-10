df47a78546330050a58d738ef1de90cfb1f4bca9
Amlal El Mahrouss <amlal@nekernel.org>
2026-03-10 11:57:34 +0100

    [IMPORTANT] Add header ontop of file.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- src/libPOSIXWrapper/POSIXKit/unistd.h
+++ src/libPOSIXWrapper/POSIXKit/unistd.h
@@ -1,3 +1,7 @@
+// Copyright 2026, Amlal El Mahrouss (amlal@nekernel.org)
+// Licensed under the Apache License, Version 2.0 (see LICENSE file)
+// Official repository: https://github.com/ne-foss-org/nekernel
+
 #ifndef LIBPOSIX_POSIXKIT_UNISTD_H
 #define LIBPOSIX_POSIXKIT_UNISTD_H
 

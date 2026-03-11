269012b320d0b81df07a78c1082a9f7b2f106df3
Amlal El Mahrouss <amlal@nekernel.org>
2026-03-11 16:20:44 +0100

    [CHORE] Add SPDX header for files.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- src/kernel/HALKit/AMD64/PCI/DMA.cpp
+++ src/kernel/HALKit/AMD64/PCI/DMA.cpp
@@ -1,7 +1,7 @@
-// Copyright 2024-2025, Amlal El Mahrouss (amlal@nekernel.org)
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2024-2026, Amlal El Mahrouss (amlal@nekernel.org)
 // Licensed under the Apache License, Version 2.0 (see LICENSE file)
 // Official repository: https://github.com/ne-foss-org/nekernel
-
 #include <ArchKit/ArchKit.h>
 #include <KernelKit/PCI/DMA.h>
 
--- src/kernel/src/Ref.cpp
+++ src/kernel/src/Ref.cpp
@@ -1,5 +1,5 @@
-// Copyright 2024-2025, Amlal El Mahrouss (amlal@nekernel.org)
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2024-2026, Amlal El Mahrouss (amlal@nekernel.org)
 // Licensed under the Apache License, Version 2.0 (see LICENSE file)
 // Official repository: https://github.com/ne-foss-org/nekernel
-
 #include <NeKit/Ref.h>

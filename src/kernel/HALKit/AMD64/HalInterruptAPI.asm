984e7503e0c7605f60dac3e8d2de40d3fdf36eae
Amlal El Mahrouss <amlal@nekernel.org>
2026-03-14 14:29:05 +0100

    [CHORE] Update assembly stubs comment.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- src/kernel/HALKit/AMD64/HalInterruptAPI.asm
+++ src/kernel/HALKit/AMD64/HalInterruptAPI.asm
@@ -1,13 +1,7 @@
-;; /*
-;; *    ---------------------------------------------------
-;; *
-;; * 	Copyright (C) 2024-2025, Amlal El Mahrouss, licensed under the Apache 2.0 license.
-;; *
-;; *    File: HalInterruptAPI.asm
-;; *    Purpose: Interrupt API, redirect raw interrupts into their handlers.
-;; *
-;; *    ---------------------------------------------------
-;; */
+;; // SPDX-License-Identifier: Apache-2.0
+;; // Copyright 2024-2026, Amlal El Mahrouss (amlal@nekernel.org)
+;; // Licensed under the Apache License, Version 2.0 (see LICENSE file)
+;; // Official repository: https://github.com/ne-foss-org/nekernel
 
 [bits 64]
 

33a87e5cce4b8d1e64a842df8fbb2acc2f89cde8
Amlal El Mahrouss <amlal@nekernel.org>
2026-03-15 06:24:35 +0100

    [CHORE] Add PThread library.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

---
+++ src/libPThread/PThreadKit/pthread.h
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2026, Amlal El Mahrouss (amlal@nekernel.org)
+// Licensed under the Apache License, Version 2.0 (see LICENSE file)
+// Official repository: https://github.com/ne-foss-org/nekernel
+
+#ifndef LIBPOSIX_POSIXKIT_THREAD_H
+#define LIBPOSIX_POSIXKIT_THREAD_H
+
+#include <libPOSIXWrapper/POSIXKit/unistd.h>
+
+#define PTHREAD_UNSAFE /* hint */
+#define PTHREAD_SAFE /* hint */
+
+#endif  // LIBPOSIX_POSIXKIT_THREAD_H

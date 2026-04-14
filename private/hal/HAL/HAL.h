5723f997e1b47e289bd93b2cac537c09314b3f78
Amlal <amlal@nekernel.org>
2026-04-14 09:01:31 +0200

    [FEAT] src: CI/CD fixes.

--- private/hal/HAL/HAL.h
+++ private/hal/HAL/HAL.h
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2026, Amlal El Mahrouss (amlal@nekernel.org)
 // Licensed under the Apache License, Version 2.0 (see LICENSE file)
-// Official repository: https://github.com/ne-foss-org/ne-kernel
+// Official repository: https://github.com/ne-foss/ne-kernel
 
 #ifndef HAL_HAL_H
 #define HAL_HAL_H

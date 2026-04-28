747bf0ef9b0c006a65ec75744986b168f319d2a0
Amlal <amlal@nekernel.org>
2026-04-28 15:00:42 +0200

    [FEAT] Introduce DetectKit for `mindetect`.

---
+++ private/mindetect/DetectKit/DetectKit.h
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2026, Amlal El Mahrouss (amlal@nekernel.org)
+// Licensed under the Apache License, Version 2.0 (see LICENSE file)
+// Official repository: https://github.com/ne-foss-org/nekernel
+
+#pragma once
+
+#include <BootKit/BootKit.h>
+#include <BootKit/BootThread.h>
+#include <BootKit/HW/SATA.h>
+#include <FirmwareKit/EFI.h>
+#include <FirmwareKit/EFI/API.h>
+#include <FirmwareKit/Handover.h>
+#include <KernelKit/MSDOS.h>
+#include <KernelKit/PE.h>
+#include <KernelKit/PEF.h>
+#include <NeKit/Macros.h>
+#include <NeKit/Ref.h>
+#include <modules/CoreGfx/CoreGfx.h>
+#include <modules/CoreGfx/TextGfx.h>
+
+#ifndef __NE_ONLY_SMP__
+#ifndef __NE_NO_SMP__
+#define __NE_ONLY_SMP__
+#endif
+#endif
+
--- private/mindetect/src/OSDetect.cpp
+++ private/mindetect/src/OSDetect.cpp
@@ -3,19 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (see LICENSE file)
 // Official repository: https://github.com/ne-foss-org/nekernel
 
-#include <BootKit/BootKit.h>
-#include <BootKit/BootThread.h>
-#include <BootKit/HW/SATA.h>
-#include <FirmwareKit/EFI.h>
-#include <FirmwareKit/EFI/API.h>
-#include <FirmwareKit/Handover.h>
-#include <KernelKit/MSDOS.h>
-#include <KernelKit/PE.h>
-#include <KernelKit/PEF.h>
-#include <NeKit/Macros.h>
-#include <NeKit/Ref.h>
-#include <modules/CoreGfx/CoreGfx.h>
-#include <modules/CoreGfx/TextGfx.h>
+#include <mindetect/DetectKit/DetectKit.h>
 
 EXTERN_C Int32 OSDetectModuleMain(Kernel::HEL::BootInfoHeader* handover) {
   ::fw_init_efi((EfiSystemTable*) handover->f_FirmwareCustomTables[Kernel::HEL::kHandoverTableST]);

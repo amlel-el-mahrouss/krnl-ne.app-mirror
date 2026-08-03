cabb6f351f8c23a2e76210747a24b4c536dfaea5
Amlal El Mahrouss <amlal@nekernel.org>
2026-08-03 07:36:21 +0200

    [CHORE] src: updated minkernel module for Jails.cpp.
    Signed-off-by: Amlal El Mahrouss <amlal@nekernel.org>

--- private/libSystem/src/Jail.cpp
+++ private/libSystem/src/Jail.cpp
@@ -3,13 +3,8 @@
 // Licensed under the Apache License, Version 2.0 (see LICENSE file)
 // Official repository: https://github.com/ne-foss/ne-kernel
 
-#include <SystemKit/Err.h>
 #include <SystemKit/Jail.h>
 #include <SystemKit/Syscall.h>
-#include <SystemKit/System.h>
-#include <SystemKit/Verify.h>
-
-using namespace System;
 
 IMPORT_C struct JAIL* JailGetCurrent(Void) {
   return (struct JAIL*) nesys_syscall_arg_1(SYSCALL_HASH("JailGetCurrent"));

17559c693246161fcad9318474b54576bae3b3c5
Amlal El Mahrouss <amlal@nekernel.org>
2026-08-18 16:57:08 +0200

    FEAT: add DHCP and CDFS drivers with initial implementation

---
+++ private/drivers/DHCP.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2024-2026, Amlal El Mahrouss (amlal@nekernel.org)
+// Licensed under the Apache License, Version 2.0 (see LICENSE file)
+// Official repository: https://github.com/ne-app-eu/krnl
+
+#include <modules/DHCP/DHCP.h>
+
+/// @brief NeSystem and ANT DHCP driver.
+/// @note This driver is used to only connect using a DHCP host behind the workstation. To avoid
+/// nightmare scenarios that could have been avoided.
+

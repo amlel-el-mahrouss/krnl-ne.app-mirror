ba335669a4bde7f6d455c852ce136c8412ce2145
Amlal El Mahrouss <amlal@nekernel.org>
2026-08-19 11:34:21 +0200

    FEAT: enhance DHCP driver and improve team switching logic in UserProcessScheduler

--- private/drivers/DHCP.c
+++ private/drivers/DHCP.c
@@ -3,9 +3,13 @@
 // Licensed under the Apache License, Version 2.0 (see LICENSE file)
 // Official repository: https://github.com/ne-app-eu/krnl
 
+#include <SystemKit/System.h>
 #include <modules/DHCP/DHCP.h>
 
 /// @brief NeSystem and ANT DHCP driver.
 /// @note This driver is used to only connect using a DHCP host behind the workstation. To avoid
 /// nightmare scenarios that could have been avoided.
 
+/// @brief Implemented in driver to implement driver.
+struct DHCP_PRIV_CONFIG_HDR;
+struct DHCP_PRIV_HDR;
